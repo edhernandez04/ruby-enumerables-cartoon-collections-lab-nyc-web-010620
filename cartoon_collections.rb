2400c7f5a068c320a69ce2fadb896a06edcc1207
Eddie Hernandez <mchh.004@gmail.com>
2020-01-03 17:59:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find { |cheese| cheese_types.include?(cheese)}
 end
